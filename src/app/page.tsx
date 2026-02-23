4f847b9aafe2f61ef596947f698a609d0ca3a4a2
deepak kahar <kahardeepak14@gmail.com>
2026-02-24 00:36:00 +0530

    implemented better auth sign in email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import { auth } from "@/lib/auth";
 import { api } from "@/lib/eden";
 
 export default async function Home() {
   const res = await api.get();
 
+  // const authresponse = await auth.api.signUpEmail({
+  //   body: {
+  //     name: "hola amigo",
+  //     username: "yolahi",
+  //     displayUsername: "yolayola",
+  //     email: "hello@example.com",
+  //     password: "hellohello",
+  //   },
+  //   asResponse: true,
+  // });
+
+  // const authresponse = await auth.api.signInEmail({
+  //   body: {
+  //     email: "hello@example.com",
+  //     password: "hellohello",
+  //   },
+  //   asResponse: true,
+  // });
+
+  // console.log(authresponse);
   return (
     <div>
       <h1>hello {res.data}</h1>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,20 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import { username } from "better-auth/plugins";
 import { db } from "@/server/drizzle/db";
+import * as schema from "@/server/models";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: {
+      ...schema,
+      user: schema.UserTable,
+      session: schema.SessionTable,
+      verification: schema.VerificationTable,
+      account: schema.AccountTable,
+    },
   }),
   emailAndPassword: {
     enabled: true,
@@ -14,5 +23,6 @@ export const auth = betterAuth({
     username({
       minUsernameLength: 5,
     }),
+    nextCookies(),
   ],
 });
